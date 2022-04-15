02c2bd4d9943f977181e50413b5cc82fbfd9baff
mikicola <michela.colangelo@gmail.com>
2022-04-15 13:28:54 +0200

    snack 1 - creo array senza errori

--- index.php
+++ index.php
@@ -1,6 +1,56 @@
-<!-- Snack 1
-Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
-Olimpia Milano - Cantù | 55-60 -->
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snacks PHP</title>
+</head>
+<body>
+    <section class="snack-1">
+        <!-- Snack 1
+        Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
+        Olimpia Milano - Cantù | 55-60 -->
+        <h1>Snack 1</h1>
+        <h2>Risultati partite</h2>
+        <?php
+        $partite = array(
+            [
+                'casa' =>[
+                    'squadra' => 'Milano',
+                    'punteggio' => 55,
+                ],
+
+                'ospite' =>[
+                    'squadra' => 'Cantù',
+                    'punteggio' => 60,
+                ],
+            ],
+
+            [
+                'casa' =>[
+                    'squadra' => 'Roma',
+                    'punteggio' => 45,
+                ],
+
+                'ospite' =>[
+                    'squadra' => 'Napoli',
+                    'punteggio' => 40,
+                ],
+            ],
+        );
+
+
+        //es prova
+        $foo = array(
+            'bar' => 'baz');
+        echo "Hello {$foo['bar']}!"; // Hello baz!
+        ?>
+    </section>
+    
+</body>
+</html>
+
 
 <!-- Snack 2
 Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione)
